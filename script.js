52a06ad6e5c3c173f75520d9aeae2162e2ff654b
Souparnoo <kundusouparna@gmail.com>
2025-08-09 13:11:17 +0530

    server deploy

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('rakhi-form');
     const rakhiList = document.getElementById('rakhi-list');
-    const API_URL = 'http://localhost:3000/api/messages';
+    const API_URL = 'https://rakhi-backend.onrender.com';
 
     // Function to fetch and render all messages from the server
     async function fetchAndRenderRakhis() {
